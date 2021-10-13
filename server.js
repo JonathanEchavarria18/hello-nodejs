73e8ba45d4bca827647d3a991250726809699547
JonathanEchavarria18 <31713108+JonathanEchavarria18@users.noreply.github.com>
2021-10-12 22:44:44 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require ('http');
+
+const port = 82;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Conten-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, ()=> }
+  console.log(´server running on port: ${port}´);
+});
